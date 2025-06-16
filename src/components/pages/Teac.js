0ce0a15a2c9e0083235964afa019161ff7071d36
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-06-16 19:06:26 +0500

    nothing

--- src/components/pages/Teac.js
+++ src/components/pages/Teac.js
@@ -118,9 +118,9 @@ export default function Teac() {
                  <label  htmlFor="fname" className='ms-1 fs-6 fw-semibold'>DESCRIBE YOUR EVENT (Are there any additional details you would like us to be aware of?)*</label><br></br>
                  <textarea className="form-control rounded-3 " id="exampleFormControlTextarea1" rows="5"></textarea><br></br>
              </div>
-             <div className="d-flex flex-column mx-1 " style={{width:"98%",height:"100px"}}>
+             <div className="d-flex flex-column mx-1" style={{width:"98%",height:"100px"}}>
                  <label  htmlFor="fname" className='ms-1 fs-6 fw-semibold'>How Did You Hear About Us? *</label><br></br>
-                 <textarea className="form-control rounded-3 pt-1" id="exampleFormControlTextarea1" rows="1"></textarea><br></br>
+                 <textarea className="form-control rounded-3" id="exampleFormControlTextarea1" rows="1"></textarea><br></br>
              </div>
              <div className="pt-3" style={{height:"80px"}}>
               <button className="btn btn-primary rounded-5 fs-5 fw-normal " style={{height:"50px",width:"120px"}}>SUBMIT</button>
