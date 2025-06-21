abc6d280207a90ffeaf4467e6e2e48729ab82607
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-06-21 18:25:26 +0500

    Update Protein.js content

--- src/components/pages/Teac.js
+++ src/components/pages/Teac.js
@@ -3,7 +3,7 @@
 export default function Teac() {
   return (
     <>
-      <div className={` d-flex ${window.innerWidth  <= 900 ? "flex-column":"flex-row"} bg-black`} style={{height:window.innerWidth <= 900 ?"1100px":"850px",width:"100%",border:"2px solid red"}}>
+      <div className={` d-flex ${window.innerWidth  <= 900 ? "flex-column":"flex-row"} bg-black`} style={{height:window.innerWidth <= 900 ?"1200px":"850px",width:"100%",border:"2px solid red"}}>
          <div className="d-flex " style={{height:window.innerWidth <= 900 ?"500px":"900px",width:window.innerWidth <= 900 ?"90%":"40%",margin:window.innerWidth <= 750 ?"20% 10% 0% 10%":window.innerWidth > 750 && window.innerWidth > 750 ?"10% 15% 0% 15%":"80px 0px 0px 80px",border:"2px solid red"}}>
            <img src="teac.png" alt="T-logo" style={{height:"450px",width:"90%",padding:window.innerWidth <= 750 ?"10% 5% 5% 5%":window.innerWidth > 750 && window.innerWidth > 750 ?"10% 15% 0% 0%":"10% 5% 5% 5%",objectFit:"cover",border:"2px solid red"}}/>
          </div>
@@ -12,18 +12,18 @@ export default function Teac() {
             <div className={`d-flex ${window.innerWidth <= 900 ?"ps-2 mt-2":"mx-3 mt-4"} flex-column text-white`} style={{border:"2px solid red"}}>
               <p className={`${window.innerWidth <= 900 ?"fw-bold fs-4":"fs-3 fw-bold"}`} style={{border:"2px solid red"}}>VIETNAMESE TEA CEREMONY (ĐÁM HỎI)</p>
             </div>
-            <p className={` ${window.innerWidth <= 900 ?"ps-1":"mx-3"} text-white`} style={{border:"2px solid red"}}>_______________________________________________________________________</p>
+            <p className={` ${window.innerWidth <= 900 ?"ps-1":"mx-3"} text-white`} style={{border:"2px solid red"}}>_________________________________________________________________</p>
             <p className={`${window.innerWidth <= 900 ?"ps-2 fs-6 fst-italic ":"mx-3 fs-5 "}  fw-semibold text-white`} style={{border:"2px solid red"}}>A Vietnamese tea ceremony, commonly known as ‘Đám Hỏi’, is a profound blend of tradition and symbolism, celebrating not only the union of two souls but also the coming together of two families.</p>
 
              <p className={` ${window.innerWidth <= 900 ?"ps-2 fs-6 fst-italic ":"mx-3 fs-5 "} fw-semibold text-white `} style={{border:"2px solid red"}}>Vietnamese Tea Ceremony Rental Policy: A $200 deposit is required to secure your Vietnamese tea ceremony rentals. This deposit will be returned in full once all rental items are returned within 48 hours of the event. If any item is not returned within that timeframe, the corresponding deposit will be forfeited. By renting our Vietnamese tea ceremony items, you agree to these terms.</p>
-             <p className={`${window.innerWidth <= 900 ?"ps-1":"mx-3"} text-white`} style={{border:"2px solid red"}}>_______________________________________________________________________</p>
+             <p className={`${window.innerWidth <= 900 ?"ps-1":"mx-3"} text-white`} style={{border:"2px solid red"}}>__________________________________________________________________</p>
              <div className="" style={{height:window.innerWidth <= 900 ?"80px":"100px",border:"2px solid red"}}>
                 <button className={ `${window.innerWidth <= 900 ?"ps-2 pt-1 fw-normal ":"mx-3 mt-3 fw-bold"} btn btn-primary fs-6`} style={{height:"60px",width:window.innerWidth <= 900 ?"320px":"400px",borderRadius:"35px"}}>VIEW VIETNAMESE TEA CERMONY MENU</button>
              </div>
          </div>
       </div>
 {/* ----------------------------------------------------------section no 1---------------------------------------------------------------- */}
-      <div className={`${window.innerWidth <= 900 ? "pt-3":"pt-5"}`} style={{height:window.innerWidth <= 900 ?"1320px":"1400px",width:"100%",backgroundColor:"rgb(40, 51, 65)",border:"2px solid red"}}> 
+      <div className={`${window.innerWidth <= 900 ? "pt-3":"pt-5"}`} style={{height:window.innerWidth <= 900 ?"1400px":"1400px",width:"100%",backgroundColor:"rgb(40, 51, 65)",border:"2px solid red"}}> 
 
         <div className={`d-flex flex-column ${window.innerWidth <= 900 ?"pt-0":"pt-3"} text-white align-items-center `} style={{height:window.innerWidth <= 900 ?"1300px":"1200px", margin:window.innerWidth <= 900 ?"0px 20px 20px 10px":"0px 300px 50px 300px",border:"2px solid red"}}>
             <p className={`${window.innerWidth <= 900 ?" fs-5 ps-2 fw-semibold ":"fs-3 fw-normal "}`} style={{border:"2px solid red"}}>BOOK YOUR VIETNAMESE TEA CEREMONY WITH US!</p>
@@ -67,7 +67,7 @@ export default function Teac() {
                  <input style={{height:"40px"}} className="rounded-3 pb-1" type="text" id="fname" name="fname" placeholder='Enter here'/><br></br>
              </div>
   {/* --------------------------------------------------------section no 3--------------------------------------------------------- */}
-            <div className="ms-2 mt-1" style={{height:"30px"}}>
+            <div className="ms-2 mt-1" style={{height:window.innerWidth <= 900 ?"50px":"30px"}}>
               <p className={`fs-6 ${window.innerWidth <= 900 ?"fst-italic fw-medium":" fw-semibold"} text-white `}>SELECT WHICH ITEMS YOU’D LIKE TO ORDER FOR YOUR EVENT: *</p>
             </div>
             <div className="d-flex flex-row pt-2" style={{width:"100%",border:"2px solid red"}}>
