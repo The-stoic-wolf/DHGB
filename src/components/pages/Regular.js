3e469ee4d94383b601d1d899f65dba75c22860d3
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-06-22 20:51:48 +0500

    changes

--- src/components/pages/Regular.js
+++ src/components/pages/Regular.js
@@ -148,7 +148,7 @@ export default function Regular() {
       </div>
       <div className="bg-black h-1"style={{width:"100%"}}>
          <p className=" text-white  d-none d-lg-block"style={{margin:"0px 0px 0px 250px",padding:"0px 250px 0px 0px"}}>_______________________________________________________________________________________________________________________________________________________________________</p>
-          <p className={`${window.innerWidth <= 992 ? "text-white w-98":"d-none"}`} style={{margin:"0px 0px 0px 100px",padding:"0px 0px 0px 0px"}}>___________________________________________________</p>
+         <p className={`${window.innerWidth <= 992 ? "text-white w-98":"d-none"}`} style={{margin:"0px 0px 0px 60px"}}>_______________________________________</p>
       </div>
       {/* -----------------------------------------------------------------------------section no 3---------------------------------------------------------------------------------------- */}
        <div className="d-flex flex-row  w-100 bg-black" style={{height:window.innerWidth <= 576 ?"1200px":window.innerWidth >576 && window.innerWidth <=992 ?" 800px":"540px"}}>
@@ -183,7 +183,7 @@ export default function Regular() {
        </div>
         <div className="bg-black h-1" style={{width:"100%"}}>
          <p className="text-white mb-0  d-none d-lg-block" style={{padding:"0px 0px 0px 250px"}}>______________________________________________________________________________________________________________________________________________________________________</p>
-         <p className={`${window.innerWidth <= 992 ? "text-white w-98":"d-none"}`} style={{margin:"0px 0px 0px 100px",padding:"0px 0px 0px 0px"}}>___________________________________________________</p>
+         <p className={`${window.innerWidth <= 992 ? "text-white w-98":"d-none"}`} style={{margin:"0px 0px 0px 60px"}}>_______________________________________</p>
       </div>
     {/* ------------------------------------------------------------------------------section no 4----------------------------------------------------------------------------------------- */}
       <div className="d-flex flex-row  w-100 bg-black" style={{height:window.innerWidth <= 576 ?"500px":window.innerWidth >576 && window.innerWidth <=992 ?" 500px":"540px"}}>
