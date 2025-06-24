84c1bf94d6306ca15aef94d7d04de3e088b3f915
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-06-24 12:40:44 +0500

    change

--- src/components/pages/Protein.js
+++ src/components/pages/Protein.js
@@ -10,12 +10,12 @@ export default function protein() {
           </div>
         </div>
 
-        <div className="d-flex flex-column"  style={{height:window.innerWidth <= 700 ?"850px":window.innerWidth > 700 && window.innerWidth <= 900 ?"700px":"800px",width:window.innerWidth <= 900 ?"100%":"60%",margin:window.innerWidth <= 900 ?"10px 0px 10px 5px":"150px 0px 150px 0px",border:"1px solid  red",padding:window.innerWidth <= 900 ?"0px":'0px 0px 0px 80px'}}>
+        <div className="d-flex flex-column"  style={{height:window.innerWidth <= 700 ?"850px":window.innerWidth > 700 && window.innerWidth <= 900 ?"700px":"800px",width:window.innerWidth <= 900 ?"100%":"60%",margin:window.innerWidth <= 900 ?"0px 0px 10px 5px":"150px 0px 150px 0px",border:"1px solid  red",padding:window.innerWidth <= 900 ?"0px":'0px 0px 0px 80px'}}>
             <div className="d-flex flex-column text-white" style={{height:window.innerWidth <= 576 ?"300px":window.innerWidth > 576 && window.innerWidth  <=900 ?"230px":"400px"}}>
-                <p className={` text-white ${window.innerWidth <= 900 ?"ps-3 fw-bold fs-5 ":"fw-bold fs-3"} `} >VEGAN PROTEIN TO-GO</p>
-                <p className={` ${window.innerWidth <= 900 ?"fw-semibold fs-6 px-3 ":"fw-semibold fs-5 ps-2 mt-1"}  text-white`} style={{border:"1px solid  red"}}>Customize your meals at home with our vegan ingredients.</p>
+                <p className={` text-white ${window.innerWidth <= 900 ?"px-3 fw-bold fs-4 ":"fw-bold fs-3"} `} >VEGAN PROTEIN TO-GO</p>
+                <p className={` ${window.innerWidth <= 900 ?"fw-semibold fs-6  px-3 ":"fw-semibold fs-5 ps-2 mt-1"}  text-white`} style={{border:"1px solid  red"}}>Customize your meals at home with our vegan ingredients.</p>
                 <p className="d-none d-lg-block" >___________________________________________________________________________________________</p>
-                 <p className={`${window.innerWidth <= 900 ?"mt-0 px-2":"d-none"}`} style={{height:"8px"}}>________________________________________</p>
+                 <p className={`${window.innerWidth <= 900 ?"mt-0 px-3":"d-none"}`} style={{height:"8px"}}>________________________________________</p>
                 <p className={`${window.innerWidth <= 900 ?" pb-2 fs-6 px-3 fst-italic ":"mt-2 fs-5 "} fw-normal  text-white`} >Your favorite vegan proteins are now available in convenient To-Go boxes for purchase, allowing you to create any meal you desire in the comfort of your home. Fully   prepared & ready to be added to your favorite recipes. Options include:</p>
             </div>
             <div className={`${window.innerWidth <= 900 ?"pt-0 ps-3":"pt-3 ms-2"} d-flex flex-column`} style={{height:window.innerWidth <= 400 ?"100px":window.innerWidth > 400 && window.innerWidth  <= 900 ?"100px":"130px"}}>
