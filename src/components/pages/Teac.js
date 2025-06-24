23616e7ba21874c465abd1c9b92d84e92a9f4c7e
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-06-24 12:04:22 +0500

    change

--- src/components/pages/Teac.js
+++ src/components/pages/Teac.js
@@ -3,35 +3,35 @@
 export default function Teac() {
   return (
     <>
-      <div className={` d-flex ${window.innerWidth  <= 900 ? "flex-column":"flex-row"} bg-black`} style={{height:window.innerWidth <= 900 ?"1100px":"850px",width:"100%",border:"2px solid red"}}>
-         <div className="d-flex " style={{height:window.innerWidth <= 900 ?"500px":"850px",width:window.innerWidth <= 900 ?"90%":"40%",margin:window.innerWidth <= 750 ?"20% 10% 0% 10%":window.innerWidth > 750 && window.innerWidth > 750 ?"10% 15% 0% 15%":"80px 0px 0px 80px",border:"2px solid red"}}>
-           <img src="teac.png" alt="T-logo" style={{height:"450px",width:"90%",padding:window.innerWidth <= 750 ?"10% 5% 5% 5%":window.innerWidth > 750 && window.innerWidth > 750 ?"10% 15% 0% 0%":"10% 5% 5% 5%",objectFit:"cover",border:"2px solid red"}}/>
+      <div className={` d-flex ${window.innerWidth  <= 900 ? "flex-column":"flex-row"} bg-black`} style={{height:window.innerWidth <= 900 ?"1150px":"850px",width:"100%"}}>
+         <div className="d-flex " style={{height:window.innerWidth <= 900 ?"500px":"850px",width:window.innerWidth <= 900 ?"90%":"40%",margin:window.innerWidth <= 750 ?"20% 10% 0% 10%":window.innerWidth > 750 && window.innerWidth > 750 ?"10% 15% 0% 15%":"80px 0px 0px 80px"}}>
+           <img src="teac.png" alt="T-logo" style={{height:"450px",width:"90%",padding:window.innerWidth <= 750 ?"20% 5% 5% 5%":window.innerWidth > 750 && window.innerWidth > 750 ?"10% 15% 0% 0%":"10% 5% 5% 5%",objectFit:"cover"}}/>
          </div>
 
-         <div className="d-flex flex-column" style={{height:window.innerWidth <= 900 ?"600px":"850px",width:window.innerWidth <= 900 ?"100%":"40%",margin:window.innerWidth <= 900 ?"20px 20px 10px 5px":"80px 80px 0px 100px",border:"2px solid red"}}>
+         <div className="d-flex flex-column" style={{height:window.innerWidth <= 900 ?"650px":"850px",width:window.innerWidth <= 900 ?"100%":"40%",margin:window.innerWidth <= 900 ?"20px 20px 10px 5px":"80px 80px 0px 100px"}}>
             <div className={`d-flex ${window.innerWidth <= 900 ?"ps-2 mt-2":"mx-3 mt-4"} flex-column text-white`} style={{border:"2px solid red"}}>
               <p className={`${window.innerWidth <= 900 ?"fw-semibold fs-4 px-1":"fs-3 fw-bold"}`} style={{border:"2px solid red"}}>VIETNAMESE TEA CEREMONY (ĐÁM HỎI)</p>
             </div>
             <p className={` ${window.innerWidth <= 900 ?"ps-1 d-none":"mx-3"} text-white`} style={{border:"2px solid red"}}>_________________________________________________________________</p>
-            <p className={`${window.innerWidth <= 900 ?"ps-1 fs-6 fst-italic ":"mx-3 fs-5 "}  fw-semibold text-white`} style={{height:"130px",border:"2px solid red"}}>A Vietnamese tea ceremony, commonly known as ‘Đám Hỏi’, is a profound blend of tradition and symbolism, celebrating not only the union of two souls but also the coming together of two families.</p>
+            <p className={`${window.innerWidth <= 900 ?"ps-1 fs-6 fst-italic ":"mx-3 fs-5 "}  fw-semibold text-white`} style={{height:"130px"}}>A Vietnamese tea ceremony, commonly known as ‘Đám Hỏi’, is a profound blend of tradition and symbolism, celebrating not only the union of two souls but also the coming together of two families.</p>
 
-             <p className={` ${window.innerWidth <= 900 ?"ps-1 fs-6 fst-italic":"mx-3 fs-5 "} fw-semibold text-white `} style={{height:"200px",border:"2px solid red"}}>Vietnamese Tea Ceremony Rental Policy: A $200 deposit is required to secure your Vietnamese tea ceremony rentals. This deposit will be returned in full once all rental items are returned within 48 hours of the event. If any item is not returned within that timeframe, the corresponding deposit will be forfeited. By renting our Vietnamese tea ceremony items, you agree to these terms.</p>
+             <p className={` ${window.innerWidth <= 900 ?"ps-1 fs-6 fst-italic":"mx-3 fs-5 "} fw-semibold text-white `} style={{height:"220px"}}>Vietnamese Tea Ceremony Rental Policy: A $200 deposit is required to secure your Vietnamese tea ceremony rentals. This deposit will be returned in full once all rental items are returned within 48 hours of the event. If any item is not returned within that timeframe, the corresponding deposit will be forfeited. By renting our Vietnamese tea ceremony items, you agree to these terms.</p>
              <p className={`${window.innerWidth <= 900 ?"ps-1 d-none":"mx-3"} text-white`} style={{border:"2px solid red"}}>__________________________________________________________________</p>
-             <div className={` ${window.innerWidth <= 900 ?"d-flex justify-content-left align-items-center ps-2":"pt-1"}`} style={{height:window.innerWidth <= 900 ?"80px":"100px",border:"2px solid red"}}>
+             <div className={` ${window.innerWidth <= 900 ?"d-flex justify-content-left align-items-center ps-2":"pt-1"}`} style={{height:window.innerWidth <= 900 ?"80px":"100px"}}>
                 <button className={ `${window.innerWidth <= 900 ?"ps-2 pt-1 fw-normal ":"mx-3 mt-3 fw-bold"} btn btn-primary fs-6`} style={{height:"50px",width:window.innerWidth <= 900 ?"320px":"400px",borderRadius:"35px"}}>VIEW VIETNAMESE TEA CERMONY MENU</button>
              </div>
          </div>
       </div>
 {/* ----------------------------------------------------------section no 1---------------------------------------------------------------- */}
-      <div className={`${window.innerWidth <= 900 ? "pt-3":"pt-5"}`} style={{height:window.innerWidth <= 900 ?"1700px":"1400px",width:"100%",backgroundColor:"rgb(40, 51, 65)",border:"2px solid red"}}> 
+      <div className={`${window.innerWidth <= 900 ? "pt-3":"pt-5"}`} style={{height:window.innerWidth <= 900 ?"1600px":"1400px",width:"100%",backgroundColor:"rgb(40, 51, 65)"}}> 
 
-        <div className={`d-flex flex-column ${window.innerWidth <= 900 ?"pt-0":"pt-3"} text-white align-items-center `} style={{height:window.innerWidth <= 900 ?"1300px":"1200px", margin:window.innerWidth <= 900 ?"0px 20px 20px 10px":"0px 300px 50px 300px",border:"2px solid red"}}>
+        <div className={`d-flex flex-column ${window.innerWidth <= 900 ?"pt-0":"pt-3"} text-white align-items-center `} style={{height:window.innerWidth <= 900 ?"1300px":"1200px", margin:window.innerWidth <= 900 ?"0px 20px 20px 10px":"0px 300px 50px 300px"}}>
             <p className={`${window.innerWidth <= 900 ?" fs-5 ps-2 fw-semibold ":"fs-3 fw-normal "}`} style={{border:"2px solid red"}}>BOOK YOUR VIETNAMESE TEA CEREMONY WITH US!</p>
-            <p className={`${window.innerWidth <= 900 ?"fst-italic ps-2":"fs-5 fw-normal"} `} style={{margin:window.innerWidth <= 900 ?"10px 10px 0px 10px":"10px 120px 0px 120px",border:"2px solid red"}}>At Đông Hưng Viên Bakery, we specialize in bringing to life the sweetness of  your engagement (Đám Hỏi) and wedding day with our bespoke cakes and personalized service.</p>
+            <p className={`${window.innerWidth <= 900 ?"fst-italic ps-2":"fs-5 fw-normal"} `} style={{margin:window.innerWidth <= 900 ?"10px 10px 0px 10px":"10px 120px 0px 120px"}}>At Đông Hưng Viên Bakery, we specialize in bringing to life the sweetness of  your engagement (Đám Hỏi) and wedding day with our bespoke cakes and personalized service.</p>
 
-          <div className="d-flex flex-column mt-4"  style={{height:window.innerWidth <= 900 ?"1200px":"1100px",width:"100%",border:"2px solid red"}}>
+          <div className="d-flex flex-column mt-4"  style={{height:window.innerWidth <= 900 ?"1200px":"1100px",width:"100%"}}>
 
-            <div className={`d-flex ${window.innerWidth <= 900 ? "flex-column":"flex-row"} `} style={{height:window.innerWidth <= 900 ?"150px":"70px",border:"2px solid red"}}>
+            <div className={`d-flex ${window.innerWidth <= 900 ? "flex-column":"flex-row"} `} style={{height:window.innerWidth <= 900 ?"150px":"70px"}}>
                <div className={`d-flex flex-column  ps-1 `} style={{width:window.innerWidth <= 900 ?"100%":"50%",height:window.innerWidth <= 900 ?"70px":"0px"}}>
                  <label  htmlFor="fname" className={`fs-5 fw-normal px-1 pb-1`} >First name:</label>
                  <input className="rounded-3 mb-1" type="text" id="fname" name="fname"  style={{width:window.innerWidth <= 900 ?"99%":'97%'}} placeholder='Enter here'/>
@@ -70,8 +70,8 @@ export default function Teac() {
             <div className="ms-2 mt-1" style={{height:window.innerWidth <= 900 ?"50px":"30px"}}>
               <p className={`fs-6 ${window.innerWidth <= 900 ?"fst-italic fw-medium":" fw-semibold"} text-white `}>SELECT WHICH ITEMS YOU’D LIKE TO ORDER FOR YOUR EVENT: *</p>
             </div>
-            <div className="d-flex flex-row pt-2" style={{width:"100%",border:"2px solid red"}}>
-                <div className="d-flex flex-column "style={{width:window.innerWidth <= 600 ?"90%":"50%",border:"2px solid red"}}>
+            <div className="d-flex flex-row pt-2" style={{width:"100%"}}>
+                <div className="d-flex flex-column "style={{width:window.innerWidth <= 600 ?"90%":"50%"}}>
                   <div className={`${window.innerWidth <= 900 ? "pt-1":"pt-0"} d-flex px-2`}> 
                     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"  style={{transform: "scale(1.5)",}}/>
                     <label htmlFor="vehicle1"className={`${window.innerWidth <= 900 ?"fs-6 ":"fs-5"} ms-2 fw-semibold`}>BÁNH CỐM</label>
@@ -93,7 +93,7 @@ export default function Teac() {
                    </div>
                  </div>  
 
-                 <div className="d-flex flex-column "style={{width:window.innerWidth <= 600 ?"90%":"50%",border:"2px solid red"}}>
+                 <div className="d-flex flex-column "style={{width:window.innerWidth <= 600 ?"90%":"50%"}}>
                   <div className={`${window.innerWidth <= 900 ? "pt-1":"pt-0"} d-flex px-2`}> 
                     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"  style={{transform: "scale(1.5)",}}/>
                     <label htmlFor="vehicle1"className={`${window.innerWidth <= 900 ?"fs-6 ":"fs-5"} ms-2 fw-semibold`}>BÁNH XU-XÊ</label>
@@ -104,12 +104,12 @@ export default function Teac() {
                      <label htmlFor="vehicle1"className={`${window.innerWidth <= 900 ?"fs-6 ":"fs-5"} ms-2 fw-semibold`}>XÔI TRÁI TIM</label>
                    </div>
 
-                    <div className={`${window.innerWidth <= 900 ? "pt-1":"pt-0"} d-flex px-2`}>
+                    <div className={`${window.innerWidth <= 900 ? "pt-2":"pt-0"} d-flex px-2`}>
                     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"  style={{transform: "scale(1.5)",}}/>
                     <label htmlFor="vehicle1"className={`${window.innerWidth <= 900 ?"fs-6 ":"fs-5"} ms-2 fw-semibold`}>KHAY RƯỢU</label>
                    </div>
 
-                    <div className={`${window.innerWidth <= 900 ? "pt-1":"pt-0"} d-flex px-2`}>
+                    <div className={`${window.innerWidth <= 900 ? "pt-2":"pt-0"} d-flex px-2`}>
                     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" style={{transform: "scale(1.5)",}}/>
                     <label htmlFor="vehicle1"className={`${window.innerWidth <= 900 ?"fs-6 ":"fs-5"} ms-2 fw-semibold`}>MÂM QUẢ</label>
                    </div>
@@ -119,12 +119,12 @@ export default function Teac() {
                  <label  htmlFor="fname" className={`${window.innerWidth <= 900 ?"pt-1 pb-2 fst-italic fw-normal":"pt-1 fw-semibold"} ms-1 fs-6 `}>DESCRIBE YOUR EVENT (Are there any additional details you would like us to be aware of?)*</label>
                  <textarea className="form-control rounded-3" id="exampleFormControlTextarea1" rows="5"></textarea>
              </div>
-             <div className="d-flex flex-column mx-1 pt-2" style={{width:"98%",height:"100px",border:"2px solid red"}}>
+             <div className="d-flex flex-column mx-1 pt-2" style={{width:"98%",height:"100px"}}>
                  <label  htmlFor="fname" className='ms-1 fs-6 fw-semibold pb-1'>How Did You Hear About Us? *</label>
                  <textarea className="form-control rounded-3" id="exampleFormControlTextarea1" rows="1"></textarea>
              </div>
-             <div className={`${window.innerWidth <= 900 ?"pt-1":"pt-3"}`} style={{height:window.innerWidth <= 900 ?"60px":"80px",border:"2px solid red"}}>
-              <button className={`${window.innerWidth <= 900 ?"fs-6":"fs-5"} btn btn-primary rounded-5  fw-normal `} style={{height:"50px",width:window.innerWidth <= 900 ?"100px":"120px"}}>SUBMIT</button>
+             <div className={`${window.innerWidth <= 900 ?"pt-1":"pt-3"}`} style={{height:window.innerWidth <= 900 ?"60px":"80px"}}>
+              <button className={`${window.innerWidth <= 900 ?"fs-6":"fs-5"} btn btn-primary rounded-5  fw-normal `} style={{height:"55px",width:window.innerWidth <= 900 ?"100px":"120px"}}>SUBMIT</button>
              </div>
             </div>
          </div>
