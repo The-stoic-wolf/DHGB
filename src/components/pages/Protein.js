487d481f0d8fbf31dd97d7f87f4722978d2238a8
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-07-13 03:22:36 +0500

    changes

--- src/components/pages/Protein.js
+++ src/components/pages/Protein.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function protein() {
   return (
     <>
-    <div className={`d-flex ${window.innerWidth <= 900 ?"flex-column":"flex-row flex-wrap"} bg-warning w-100 bg-black h-100`} style={{height:window.innerWidth <= 900 ? "1550px":"1100px",objectFit:"cover"}}>
-        <div style={{height:window.innerWidth <= 700 ?"450px":window.innerWidth > 700 && window.innerWidth <= 900 ?"450px":"800px",width:window.innerWidth <= 700 ?"100%":window.innerWidth > 700 && window.innerWidth <= 900 ?"80%":"40%", margin :window.innerWidth <= 700 ?"100px 0px 0px 0px":window.innerWidth > 700 && window.innerWidth  <=900 ?"50px 0px 0px 50px":"150px 0px 150px 0px",padding:window.innerWidth <= 700 ?"15% 15% 10% 15%":window.innerWidth > 700 && window.innerWidth <= 900 ?"15% 5% 10% 5%":"0px 0px 0px 100px"}}>
+    <div className={`d-flex ${window.innerWidth <= 900 ?"flex-column":"flex-row flex-wrap"} bg-warning w-100 bg-black h-100`} style={{height:window.innerWidth <= 900 ? "1550px":"1200px",objectFit:"cover"}}>
+        <div style={{height:window.innerWidth <= 700 ?"450px":window.innerWidth > 700 && window.innerWidth <= 900 ?"450px":"850px",width:window.innerWidth <= 700 ?"100%":window.innerWidth > 700 && window.innerWidth <= 900 ?"80%":"40%", margin :window.innerWidth <= 700 ?"100px 0px 0px 0px":window.innerWidth > 700 && window.innerWidth  <=900 ?"50px 0px 0px 50px":"150px 0px 150px 0px",padding:window.innerWidth <= 700 ?"15% 15% 10% 15%":window.innerWidth > 700 && window.innerWidth <= 900 ?"15% 5% 10% 5%":"0px 0px 0px 100px"}}>
           <div className="d-flex " style={{margin:window.innerWidth <= 900 ?"0px":"40px 0px 0px 0px",padding:window.innerWidth <= 900 ?"0px":"0px 0px 0px 40px",height:window.innerWidth <= 900 ?"300px":"500px"}}>
                <img className="h-100 w-100 img-fluid" style={{objectFit:"cover",width:"100%"}} src="pro-to-go.png" alt="" />
           </div>
         </div>
 
-        <div className="d-flex flex-column"  style={{height:window.innerWidth <= 700 ?"850px":window.innerWidth > 700 && window.innerWidth <= 900 ?"700px":"800px",width:window.innerWidth <= 900 ?"100%":"60%",margin:window.innerWidth <= 900 ?"10px 0px 10px 0px":"150px 0px 150px 0px",padding:window.innerWidth <= 900 ?"0px":'0px 0px 0px 80px'}}>
+        <div className="d-flex flex-column"  style={{height:window.innerWidth <= 700 ?"850px":window.innerWidth > 700 && window.innerWidth <= 900 ?"700px":"850px",width:window.innerWidth <= 900 ?"100%":"60%",margin:window.innerWidth <= 900 ?"10px 0px 10px 0px":"150px 0px 150px 0px",padding:window.innerWidth <= 900 ?"0px":'0px 0px 0px 80px'}}>
             <div className="d-flex flex-column text-white" style={{height:window.innerWidth <= 576 ?"300px":window.innerWidth > 576 && window.innerWidth  <=900 ?"230px":"400px"}}>
                 <p className={` text-white ${window.innerWidth <= 900 ?"px-3 fw-bold fs-4 ":"fw-bold fs-3"} `} >VEGAN PROTEIN TO-GO</p>
                 <p className={` ${window.innerWidth <= 900 ?"fw-semibold fs-6  px-3 ":"fw-semibold fs-5 ps-2 mt-1"}  text-white`} >Customize your meals at home with our vegan ingredients.</p>
@@ -54,7 +54,7 @@ export default function protein() {
             </div>
             <p className="text-white mt-2 d-none d-lg-block">___________________________________________________________________________________________</p>
              <p className={`${window.innerWidth <= 900 ?"ps-3 text-white":"d-none"}`} >_______________________________________</p>
-             <div className="ms-5">
+             <div className={`${window.innerWidth <= 900 ?"ms-3 pt-0 mt-0":""}`}>
                <button type="button" className={`${window.innerWidth <= 700 ?"px-3 mt-2 pb-1 fw-semibold fs-6 btn btn-primary ":window.innerWidth > 700 && window.innerWidth <= 900 ?"px-1 mt-0 fw-semibold fs-6 btn btn-primary ":"mt-3 fw-bold fs-5 btn btn-primary "} `}  style={{height:window.innerWidth <= 900 ?"50px":"70px" , width:window.innerWidth <= 900 ?"250px":"300px",borderRadius:"70px"}}>View All Vegan Bánh Mì</button>
              </div>
         </div>
